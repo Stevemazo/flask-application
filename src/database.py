edf805d797a4614cdc6da6e19c5c38a6be6bb507
Stevemazo <142940085+Stevemazo@users.noreply.github.com>
2025-03-07 18:43:34 +0100

    Modification de variable dans database.py

--- src/database.py
+++ src/database.py
@@ -5,7 +5,7 @@ import urllib.parse as urlparse
 
 DATABASE_URL = os.getenv('mysql://uep6l1uhu56nkbi3:CNacQRq1VtTrNx01CoX6@btz7zr3tt1erxfbygo4e-mysql.services.clever-cloud.com:3306/btz7zr3tt1erxfbygo4e')
 
-if not DATABASE_URI: raise ValueError("La variable d'environnement MYSQL_ADDON_URI est introuvable")
+if not DATABASE_URL: raise ValueError("La variable d'environnement MYSQL_ADDON_URI est introuvable")
 
 url = urlparse.urlparse(DATABASE_URL)
 
