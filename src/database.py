4ff1056c9f7fbd2bd232a907c9ae2b66aef25686
Stevemazo <142940085+Stevemazo@users.noreply.github.com>
2025-03-07 16:47:26 +0100

    Update database.py

--- src/database.py
+++ src/database.py
@@ -13,6 +13,6 @@ database = mysql.connector.connect(
     host='btz7zr3tt1erxfbygo4e-mysql.services.clever-cloud.com',
     user='uep6l1uhu56nkbi3',
     password='CNacQRq1VtTrNx01CoX6',
-    database='btz7zr3tt1erxfbygo4e'
+    database='btz7zr3tt1erxfbygo4e',
     port='3306'
 )
