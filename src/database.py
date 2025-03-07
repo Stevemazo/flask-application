1703534be0729571cc80d4f7742ef10e5b2ca289
Stevemazo <142940085+Stevemazo@users.noreply.github.com>
2025-03-07 10:30:05 +0100

    initialiser flask-application

---
+++ src/database.py
@@ -0,0 +1,8 @@
+import mysql.connector
+
+database = mysql.connector.connect(
+    host='btz7zr3tt1erxfbygo4e-mysql.services.clever-cloud.com',
+    user='uep6l1uhu56nkbi3',
+    password='CNacQRq1VtTrNx01CoX6',
+    database='btz7zr3tt1erxfbygo4e'
+)
