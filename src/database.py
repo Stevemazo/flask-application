a2975472117c008cd381f2586e04398a30119aa4
Stevemazo <142940085+Stevemazo@users.noreply.github.com>
2025-03-07 18:50:04 +0100

    Deuxième Modification de variable dans database.py

--- src/database.py
+++ src/database.py
@@ -1,13 +1,4 @@
-import os
 import mysql.connector
-import urllib.parse as urlparse
-
-
-DATABASE_URL = os.getenv('mysql://uep6l1uhu56nkbi3:CNacQRq1VtTrNx01CoX6@btz7zr3tt1erxfbygo4e-mysql.services.clever-cloud.com:3306/btz7zr3tt1erxfbygo4e')
-
-if not DATABASE_URL: raise ValueError("La variable d'environnement MYSQL_ADDON_URI est introuvable")
-
-url = urlparse.urlparse(DATABASE_URL)
 
 database = mysql.connector.connect(
     host='btz7zr3tt1erxfbygo4e-mysql.services.clever-cloud.com',
